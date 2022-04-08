d8e9612374f20fe0b9fc5581a30b903d35cc11f2
Seungil <seungilbang@khu.ac.kr>
2022-04-08 11:00:28 +0900

    modified

--- gugudan.py
+++ gugudan.py
@@ -1,5 +1,6 @@
-for x in range(2, 9):
+for x in range(1, 10):
     print("------- [" + str(x) + "단] -------")
     for y in range(1, 10):
         print(x, "X", y, "=", x*y)
 print("---------------------")
+# 9단 수정 완료!
